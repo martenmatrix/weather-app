9916987fb5fe2f6f96a22dba62638965f1090712
Marten <derapfelesser@aol.com>
2021-10-01 23:39:58 +0200

    Fullscreen video autoplaying

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
