abd81700974c1e17e4c160b3d6b487ce77368b91
Marten <derapfelesser@aol.com>
2021-10-01 23:43:00 +0200

    Remove minimzer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
