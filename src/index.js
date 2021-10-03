60d23ec68ba4b9be2e82fa8f8a5f91d1a07dcab4
Marten <derapfelesser@aol.com>
2021-10-04 01:44:20 +0200

    Add basic weather layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import css from './style.css';
+
+class WeatherInfo {
+    static OpenWeatherAPIKEY = 'd6ba7bd5f4bb9861d8b78c0e508d31eb';
+    static units = 'metric';
+
+    static getDataForCity(city) {
+        const url = `api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${this.OpenWeatherAPIKEY}`;
+    }
+}
+
+class DOM {
+// weather codes https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+//sand has no icon//ash has no icon//squall has no icon
+}
