57c1244ca9c2b5971b58aeff95e13f913c7df97d
Marten <derapfelesser@aol.com>
2021-10-06 00:09:50 +0200

    Re-arragend files

---
+++ src/assets.js
@@ -0,0 +1,6 @@
+/* Import videos */
+import fog1 from './videos/fog/fog_forest_drive.webm';
+import rain1 from './videos/rain/rain_window.webm';
+import snow1 from './videos/snow/snow_forest_house.webm';
+import snow2 from './videos/snow/snow_lake.webm';
+import sun1 from './videos/sun/rain_window'
