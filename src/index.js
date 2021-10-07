7e8b7f278adf841c05f4365c94ec9eb8fc5f1250
Marten <derapfelesser@aol.com>
2021-10-08 00:34:41 +0200

    Add async babel support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import css from './style.css';
 import Media from './assets.js';
 
+// do i really need to have the runtime-generator installed as dependency?
+
 class WeatherInfo {
     static OpenWeatherAPIKEY = 'd6ba7bd5f4bb9861d8b78c0e508d31eb';
     static units = 'metric';
 
     static async getDataForCity(city) {
-        const url = `api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${this.OpenWeatherAPIKEY}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${this.units}&appid=${this.OpenWeatherAPIKEY}`;
         const response = await fetch(url, {mode: 'cors'});
         const data = await response.json();
+        console.log(data);
 
         return data;
     }
@@ -22,17 +25,17 @@ class DOM {
         sourceElement.src = src;
     }
 
-    static #moveSearchToTop() {
+    static moveSearchToTop() {
         const searchbar = document.querySelector('.search-container');
         searchbar.classList.add('active');
     }
 
-    static #setInfoVisible() {
+    static setInfoVisible() {
         const weatherInfo = document.querySelector('.weather-container');
         weatherInfo.classList.add('active');
     }
 
-    static #setInfoInvisible() {
+    static setInfoInvisible() {
         const weatherInfo = document.querySelector('.weather-container');
         weatherInfo.classList.remove('active');
     }
@@ -56,4 +59,18 @@ class DOM {
         const loadingCircle = document.querySelector('.loading');
         loadingCircle.classList.toggle('active');
     }
+
+    static getInput() {
+        const searchBar = document.querySelector('.search-container .locationinput');
+        return searchBar.value;
+    }
 }
+
+WeatherInfo.getDataForCity('hamburg');
+
+
+
+document.addEventListener('click', function() {
+    DOM.moveSearchToTop();
+    DOM.toggleLoadingState();
+})
--- webpack.config.js
+++ webpack.config.js
@@ -32,29 +32,29 @@ module.exports = {
             {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
-           },
-           {
+            },
+            {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
-           },
-           {
+            },
+            {
             test: /\.(webm)$/i,
             type: 'asset/resource',
-           },
-           {
+            },
+            {
              test: /\.html$/i,
              loader: 'html-loader',
-           },
-           {
+            },
+            {
              test: /\.m?js$/,
              exclude: /(node_modules|bower_components)/,
              use: {
                loader: 'babel-loader',
                options: {
-                 presets: ['@babel/preset-env']
-               }
-             }
-           }
+                 presets: [['@babel/preset-env', {useBuiltIns: 'usage', corejs: 3}]]
+                }
+            },
+          },
       ],
   },
 
