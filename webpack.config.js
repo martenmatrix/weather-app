30f3e83c6f1b9a9b831917a22c912089d0382712
Marten <derapfelesser@aol.com>
2021-10-12 20:36:38 +0200

    Better webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -7,6 +8,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'main.js',
+    assetModuleFilename: 'assets/[hash][ext][query]',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -27,25 +29,28 @@ module.exports = {
   module: {
       rules: [
             {
-             test: /\.m?js$/,
-             exclude: /(node_modules|bower_components)/,
-             use: {
-               loader: 'babel-loader',
-               options: {
-                 presets: [['@babel/preset-env', {useBuiltIns: 'usage', corejs: 3}]],
-                 inputSourceMap: false,
-                 sourceMaps: 'both',
-                 compact: false,
-                }
-            },
-            },
-            {
              test: /\.css$/i,
-             use: ['style-loader', 'css-loader'],
+             use: ['style-loader', 'css-loader',
+                   {
+                    loader: "postcss-loader",
+                    options: {
+                      postcssOptions: {
+                        plugins: [
+                          [
+                            "postcss-preset-env",
+                            {
+                              // Options
+                            },
+                          ],
+                        ],
+                      },
+                    },
+                  }, // add sass loader after postcss loader (https://youtu.be/TOb1c39m64A?list=PLmZPx_9ZF_sB4orswXdpThGMX9ii2uP7Z&t=2155)
+            ],
             },
             {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+            type: 'asset', // determines on the size (max size is 8kb) of the file, whether it should be inlined(js in assets directory) or in the assets directory
             },
             {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -58,13 +63,29 @@ module.exports = {
             {
              test: /\.html$/i,
              loader: 'html-loader',
-            }
+            },
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [['@babel/preset-env', { "debug": true, useBuiltIns: 'usage', corejs: 3 }]],
+                  inputSourceMap: false,
+                  sourceMaps: 'both',
+                  compact: false,
+                 }
+               },
+             },
       ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-    })
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.STATS || "disabled",
+    }),
   ],
 };
